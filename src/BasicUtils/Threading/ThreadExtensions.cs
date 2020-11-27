c195c7955ead0a3d5d3ceea6f97b84386e01aab9
jamiefutch <jamiefutch@gmail.com>
2020-11-26 21:00:50 -0500

    added Threadding utils; added jetbrains rider to .gitignore

---
+++ src/BasicUtils/Threading/ThreadExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace BasicUtils.Threading
+{
+    /// <summary>
+    /// Extension methods to help with threads
+    /// </summary>
+    public static class ThreadExtensions
+    {
+            
+        /// <summary>
+        /// Waits for all threads in the threads collection to finish before executing the next instruction
+        /// Usage:
+        ///     Create a collection of threads: List<Thread> threadsList = new List<Thread>();
+        ///     Add the threads to the collection: threadsList.Add(threadX);
+        ///     At the point where waiting on threads is desired: threadsList.WaitAll();
+        /// </summary>
+        /// <param name="threads">threads collection</param>
+        public static void WaitAll(this IEnumerable<Thread> threads)
+        {
+            if(threads!=null)
+            {
+                foreach(Thread thread in threads)
+                { thread.Join(); }
+            }
+        }
+    }
+}
