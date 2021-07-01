2a5c60ffe0f1886758f89e4d06291fd3e86c47e2
Jamie Futch <jamiefutch@Jamies-MBP.attlocal.net>
2021-07-01 02:30:46 -0400

    updates to string extensions

--- src/BasicUtils/StringExtensions.cs
+++ src/BasicUtils/StringExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace BasicUtils
 {
@@ -7,6 +8,13 @@ namespace BasicUtils
     /// </summary>
     public static class StringExtensions
     {
+        /// <summary>
+        /// Sets the console foreground color to the current console foreground color
+        /// as a compromise so as not to have a null by 'default'
+        /// </summary>
+        private static ConsoleColor _cachedConsoleColor = Console.ForegroundColor;
+        
+        // ReSharper disable once MemberCanBePrivate.Global
         public static void Print(this string output, 
             bool showTimeStamp = true,
             ConsoleColor textColor = ConsoleColor.White)
@@ -25,6 +33,73 @@ namespace BasicUtils
             Console.ForegroundColor = tempColor;
         }
 
+        /// <summary>
+        /// Simple method to print output string N times with no color formatting
+        /// </summary>
+        /// <param name="output"></param>
+        /// <param name="RepeatNTimes"></param>
+        public static void Print(this string output,
+            int RepeatNTimes)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i <= RepeatNTimes; i++)
+            {
+                sb.Append(output);
+            }
+            Console.WriteLine(sb.ToString());
+        }
+        
+        /// <summary>
+        /// Prints a string N times
+        /// </summary>
+        /// <param name="output"></param>
+        /// <param name="RepeatNTimes"></param>
+        /// <param name="showTimeStamp"></param>
+        /// <param name="textColor"></param>
+        public static  void Print(this string output,
+            int RepeatNTimes,
+            bool showTimeStamp = true,
+            ConsoleColor textColor = ConsoleColor.White)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < RepeatNTimes; i++)
+            {
+                sb.Append(output);
+            }
+            
+            var tempColor = Console.ForegroundColor;
+            Console.ForegroundColor = textColor;
+            if (showTimeStamp)
+            {
+                Console.WriteLine($"{System.DateTime.Now}\t{sb.ToString()}");
+            }
+            else
+            {
+                Console.WriteLine($"{sb.ToString()}");
+            }
+            Console.ForegroundColor = tempColor;
+        }
+
+        public static string Input(this string output,
+            bool showTimeStamp = false,
+            ConsoleColor textColor = ConsoleColor.White)
+        {
+            var tempColor = Console.ForegroundColor;
+            Console.ForegroundColor = textColor;
+            if (showTimeStamp)
+            {
+                Console.Write($"{DateTime.Now.ToString()}\t{output}");
+            }
+            else
+            {
+                Console.Write(output);
+            }
+
+            var input = Console.ReadLine().Trim();
+            Console.ForegroundColor = tempColor;
+            return input.Replace(output, "");
+        }
+        
         public static void PressAnyKey(this string prompt)
         {
             if (prompt.Length == 0)
@@ -38,6 +113,45 @@ namespace BasicUtils
             Console.ReadKey();
         }
 
+        public static void PressAnyKey(this string output,
+            bool showTimeStamp = false,
+            ConsoleColor textColor = ConsoleColor.White)
+        {
+            var tempColor = Console.ForegroundColor;
+            Console.ForegroundColor = textColor;
+            if (showTimeStamp)
+            {
+                if (output.Length == 0)
+                {
+                    Console.Write($"{DateTime.Now.ToString()}\tPress any key to continue...");
+                }
+                else
+                {
+                    Console.Write($"{DateTime.Now.ToString()}\t{output}");
+                }
+            }
+            else
+            {
+                if (output.Length == 0)
+                {
+                    Console.Write($"Press any key to continue...");
+                }
+                else
+                {
+                    Console.Write($"{output}");
+                }
+            }
+            var input = Console.ReadKey();
+            Console.ForegroundColor = tempColor;    
+        }
+
+        /// <summary>
+        /// Print input prompt, returns input as string
+        /// </summary>
+        /// <param name="inputPrompt"></param>
+        /// <param name="showTimeStamp"></param>
+        /// <param name="textColor"></param>
+        /// <returns>input as string</returns>
         public static string PrintForInput(this string inputPrompt, 
             bool showTimeStamp = false,
             ConsoleColor textColor = ConsoleColor.White)
@@ -57,5 +171,24 @@ namespace BasicUtils
             Console.ForegroundColor = tempColor;
             return output.Replace(inputPrompt, "");
         }
+
+        /// <summary>
+        /// Caches current console color
+        /// </summary>
+        public static void CacheCurrentConsoleForegroundColor()
+        {
+            _cachedConsoleColor = Console.ForegroundColor;
+        }
+
+        /// <summary>
+        /// Sets console color to cached console color
+        /// </summary>
+        public static void SetConsoleForegroundToCachedColor()
+        {
+            Console.ForegroundColor = _cachedConsoleColor;
+        }
+
+        
+        
     }
 }
