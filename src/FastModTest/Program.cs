18d9b695d90ce5dccc0ec879568e62b84584929b
jamiefutch <jamiefutch@gmail.com>
2024-04-25 01:29:05 -0400

    added FastModTest for benchmarking Math.cs

---
+++ src/FastModTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using BasicUtils;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+using Console = System.Console;
+
+namespace FastModTest
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<modrunner>();
+
+        }
+
+        
+    }
+
+    
+}
---
+++ src/FastModTest/modrunner.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace FastModTest
+{
+    public class modrunner
+    {
+        const int iterations = 1000000000;
+
+        public modrunner()
+        {
+            //Console.WriteLine("Starting mod...");
+            Modrun();
+            //Console.WriteLine("Done mod.");
+
+            //Console.WriteLine("Starting mod2...");
+            Modrun2();
+            //Console.WriteLine("Done mod2.");
+
+            //Console.WriteLine("Starting mod3...");
+            Modrun3();
+            //Console.WriteLine("Done mod3.");
+        }
+
+
+        [Benchmark]
+        public void Modrun()
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < iterations; i++)
+            {
+                numbers.Add(i);
+                if (i % 100 == 0)
+                {
+                    //Console.WriteLine(i);
+                }
+            }
+        }
+
+        [Benchmark]
+        public void Modrun2()
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < iterations; i++)
+            {
+                numbers.Add(i);
+                if (BasicUtils.Math.FastModForLoop(i, 100))
+                {
+                    //Console.WriteLine(i);
+                }
+            }
+        }
+
+        [Benchmark]
+        public void Modrun3()
+        {
+            int limit = 100;
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < iterations; i++)
+            {
+                numbers.Add(i);
+                if (i > (limit - 1))
+                {
+                    //Console.WriteLine(i);
+                }
+            }
+        }
+
+    }
+}
